d18552bdf2c0d6c6ab1d204d270c4d7ea4c67ea5
longdo0612 <longdo061299@gmail.com>
2023-06-08 11:17:07 +0700

    add redux persist

--- src/app/store.js
+++ src/app/store.js
@@ -1,12 +1,35 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { apiSlice } from './api/apiSlice';
 import authReducer from '../features/auth/authSlice';
+import {
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import { storage } from 'redux-persist/lib/storage';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  version: 1,
+};
+
+const persistedReducer = persistReducer(persistConfig, authReducer);
 
 export const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
-    auth: authReducer,
+    // auth: authReducer,
+    persistedReducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(apiSlice.middleware),
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat(apiSlice.middleware),
 });
--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,15 @@ import App from './App';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
 import 'mapbox-gl/dist/mapbox-gl.css';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore } from 'redux-persist';
 
 const root = createRoot(document.querySelector('#root'));
 
 root.render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistStore(store)}>
+      <App />
+    </PersistGate>
   </Provider>
 );
